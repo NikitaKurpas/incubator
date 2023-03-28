1f0e56c06d97e4cc56b40fa704683d7858fc18c4
Nikita Kurpas <nkurpas@indeed.com>
2023-03-28 08:24:48 +0000

    styling improvements

--- trullo/src/Card.tsx
+++ trullo/src/Card.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { CardDialog } from "./CardDialog";
+import { ElementType, useState } from "react";
+// import { CardDialog } from "./CardDialog";
 import { Id, useCard } from "./data";
 import { Chip } from "./Chip";
 
@@ -9,26 +9,35 @@ export function Card({ id }: { id: Id }) {
 
   return (
     <>
-      <div
-        className="px-6 py-4 flex flex-col gap-3 bg-white rounded-lg shadow cursor-pointer"
-        onClick={() => setDetailsOpen(true)}
-      >
-        <h3 className="text-lg text-gray-800">{card.title}</h3>
+      <UICard onClick={() => setDetailsOpen(true)}>
+        <span className="text text-gray-800">{card.title}</span>
 
         {card.tags.length > 0 && (
           <div className="flex gap-2">
             {card.tags.map((tag) => (
-              <Chip tag={tag} key={tag} />
+              <Chip key={tag} label={tag} style="outline" />
             ))}
           </div>
         )}
-      </div>
+      </UICard>
 
-      <CardDialog
+      {/* <CardDialog
         id={id}
         isOpen={areDetailsOpen}
         onClose={() => setDetailsOpen(false)}
-      />
+      /> */}
     </>
+  );
+}
+
+export function UICard<T extends keyof JSX.IntrinsicElements>({
+  as: Component = "div",
+  ...rest
+}: { as?: string } & React.ComponentPropsWithoutRef<T>) {
+  return (
+    <Component
+      className="px-6 py-4 flex flex-col gap-3 bg-white border border-gray-200 rounded shadow cursor-pointer"
+      {...rest}
+    />
   );
 }
--- trullo/src/Chip.tsx
+++ trullo/src/Chip.tsx
@@ -8,8 +8,14 @@ export function Chip({
   style?: "solid" | "subtle" | "outline";
 }) {
   return (
-    <span className="px-3 py-1 text-sm text-gray-500 border border-gray-200 rounded">
+    <span className={cx("px-3 py-1 text-sm rounded", styles[style])}>
       {label}
     </span>
   );
 }
+
+const styles = {
+  subtle: "text-gray-500 bg-gray-100 border border-gray-200",
+  solid: "text-white bg-gray-500",
+  outline: "text-gray-500 border border-gray-200",
+};
